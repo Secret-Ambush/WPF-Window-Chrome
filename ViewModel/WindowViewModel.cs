7b0e67639499f0757a2b9d5925d9e3eacc219272
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-02 18:28:10 +0400

    Add files via upload

---
+++ ViewModel/WindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TheBetterWord
+{
+    public class WindowViewModel : BaseViewModel
+    {
+
+        #region Private Member
+
+        private Window mWindow;
+        private int mOuterMarginSize = 10;
+        private int mWindowRadius = 10;
+
+        #endregion
+
+        #region Public Properties
+
+        public int ResizeBorderThickness { get; set; } = 6;
+
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder); } }
+
+
+        #endregion
+
+
+
+        #region Constructor
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+        }
+        #endregion
+
+
+    }
+}
