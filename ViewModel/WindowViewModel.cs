dc23e2a5db33939815b313c68d0af1b7be4ec3a6
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-03 14:31:31 +0400

    new private members

--- ViewModel/WindowViewModel.cs
+++ ViewModel/WindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using Fasetto.Word;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace TheBetterWord
 {
@@ -20,10 +23,64 @@ namespace TheBetterWord
 
         #region Public Properties
 
-        public int ResizeBorderThickness { get; set; } = 6;
+        public double WindowMinimumWidth { get; set; } = 400;
 
-        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder); } }
+        public double WindowMinimumHeight { get; set; } = 400;
 
+        /// <summary>
+        /// The padding of the inner content of the main window
+        /// </summary>
+        public Thickness InnerContentPadding { get { return new Thickness(ResizeBorder); } }
+
+        public int ResizeBorder { get; set; } = 6;
+
+        /// <summary>
+        /// The size of the resize border around the window accounts for the outermargin thickness as well!
+        /// </summary>
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
+
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+
+        public int TitleHeight { get; set; } = 42;
+
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+        #endregion
+
+        #region Commands
+
+        public ICommand MinimizeCommand { get; set; }
+
+        public ICommand MaximizeCommand { get; set; }
+
+        public ICommand MenuCommand { get; set; }
+
+        public ICommand CloseCommand { get; set; }
 
         #endregion
 
@@ -33,7 +90,42 @@ namespace TheBetterWord
         public WindowViewModel(Window window)
         {
             mWindow = window;
+
+            // Listen out for window resizing
+            mWindow.StateChanged += (sender, e) =>
+            {
+                //fire off events for all properties that are affected by a resize
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+
+            MinimizeCommand = new RelayCommand(() => mWindow.WindowState = WindowState.Minimized);
+            MaximizeCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized); //xor
+            CloseCommand = new RelayCommand(() => mWindow.Close());
+            MenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
+
+
+            //Fix window resize issue
+            var resizer = new WindowResizer(mWindow);
         }
+        #endregion
+
+
+        #region Private Helpers
+
+        /// <summary>
+        /// Gets current mouse position on the the screen
+        /// </summary>
+        /// <returns></returns>
+        private Point GetMousePosition()
+        {
+            var position = Mouse.GetPosition(mWindow);
+            return new Point(position.X + mWindow.Left, position.Y + mWindow.Top);
+        }
+
         #endregion
 
 
